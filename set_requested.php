dcaafb65e440ff0427cbf041347e1d1010c2d27a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2019-08-25 11:10:06 +0000

    Update set_requested.php

--- set_requested.php
+++ set_requested.php
@@ -1,22 +1,32 @@
 <?php
 
+include("../db_ini.php");
 
 $data_string = NULL;
-
 $data_string = strval(getPost('returnstring', ';,,,;'));
 
-
-$servername = "mydb6.surf-town.net";
-$username = "dannil1_daq";
-$password = "2k8Y8!16";
-$dbname = "dannil1_data_logging_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if (mysqli_connect_errno())
+$conn = mysqli_init();
+if (!$conn) 
 {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  exit();
+  die('mysqli_init failed');
 }
+if (!$conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) 
+{
+  die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+}
+if (!$conn->real_connect($servername, $username, $password, $dbname)) 
+{
+  die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+//if (mysqli_connect_errno())
+//{
+//  printf("Connect failed: %s\n", mysqli_connect_error());
+//  exit();
+//}
+
 $conn->autocommit(FALSE);
 $conn->begin_transaction();
 
@@ -43,33 +53,34 @@ while ($channel_start < $data_end)
     $timestamp_end = strpos($data_string, ",", $timestamp_start);
     $timestamp_string = mb_substr($data_string, $timestamp_start, $timestamp_end-$timestamp_start);
     $timestamp = intval($timestamp_string);
-    //echo "timestamp";  
-    //echo $timestamp;
+    //error_log("timestamp");  
+    //error_log($timestamp);
     $value_start = $timestamp_end+1;
     $value_end = strpos($data_string, ",", $value_start);
     $value_string = mb_substr($data_string, $value_start, $value_end-$value_start);
     $value = doubleval($value_string);
-    //echo "value";
-    //echo $value;
+    //error_log("value");
+    //error_log($value);
     $subsamples_start = $value_end+1;
     $subsamples_end = strpos($data_string, ",", $subsamples_start);
     $subsamples_string = mb_substr($data_string, $subsamples_start, $subsamples_end-$subsamples_start);
-    //echo "subsamples_string";
-    //echo $subsamples_string;
+    //error_log("subsamples_string");
+    //error_log($subsamples_string);
     $base64_start = $subsamples_end+1;
     $base64_end = strpos($data_string, ",", $base64_start);
     $base64_string = mb_substr($data_string, $base64_start, $base64_end-$base64_start);
-    //echo "base64_string";
-    //echo $base64_string;
+    //error_log("base64_string");
+    //error_log($base64_string);
 
 
-    $stmt=$conn->prepare("UPDATE T_ACQUIRED_DATA SET ACQUIRED_VALUE=?,ACQUIRED_SUBSAMPLES=?,ACQUIRED_BASE64=?,STATUS=0 WHERE CHANNEL_INDEX=? AND ACQUIRED_TIME=? AND STATUS=-1");
+    $stmt=$conn->prepare("UPDATE " . $acquired_data_table_name . " SET ACQUIRED_VALUE=?,ACQUIRED_SUBSAMPLES=?,ACQUIRED_BASE64=?,STATUS=0 WHERE CHANNEL_INDEX=? AND ACQUIRED_TIME=? AND STATUS=-1");
     $stmt->bind_param('sssss',$value,$subsamples_string,$base64_string,$channel,$timestamp);
     if(!$stmt->execute())
     {
       $conn->rollback();
       exit();
     }
+    $stmt->close();
 
     $points_start = $base64_end+1;
   }
@@ -77,8 +88,6 @@ while ($channel_start < $data_end)
   $channel_start = $points_end+1;
 }
 
-
-$stmt->close();
 $conn->commit();
 $conn->close();
 
