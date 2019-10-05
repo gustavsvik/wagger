2399e7d6da35a63f466f6567558155a04f9f2d78
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2019-10-05 17:58:34 +0000

    Update direct_upload.php

--- direct_upload.php
+++ direct_upload.php
@@ -1,9 +1,8 @@
 <?php
 
-include("../db_ini.php");
 
-$data_string = NULL;
-$data_string = strval(getPost('returnstring', ';,,,;'));
+include("../db_ini.php");
+include("header.php");
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if (mysqli_connect_errno())
@@ -15,21 +14,21 @@ $conn->autocommit(FALSE);
 $conn->begin_transaction();
 
 $data_start = 0;
-$data_end = strlen($data_string);
+$data_end = strlen($return_string);
 $channel_start = $data_start;
 
 while ($channel_start < $data_end)
 {
   $channel = 9999;
-  $channel_end = strpos($data_string, ';', $channel_start);
-  $channel_string = mb_substr($data_string, $channel_start, $channel_end-$channel_start);
+  $channel_end = strpos($return_string, ';', $channel_start);
+  $channel_string = mb_substr($return_string, $channel_start, $channel_end-$channel_start);
   $channel = intval($channel_string);
   echo "channel";  
   echo $channel;
 
   $points_start = $channel_end+1;
-  $points_end = strpos($data_string, ';', $points_start);
-  $points_string = mb_substr($data_string, $points_start, $points_end-$points_start);
+  $points_end = strpos($return_string, ';', $points_start);
+  $points_string = mb_substr($return_string, $points_start, $points_end-$points_start);
   echo "points_start";  
   echo $points_start;
   echo "points_end";  
@@ -42,29 +41,29 @@ while ($channel_start < $data_end)
   {
     $timestamp = -9999;
     $timestamp_start = $points_start;
-    $timestamp_end = strpos($data_string, ",", $timestamp_start);
-    $timestamp_string = mb_substr($data_string, $timestamp_start, $timestamp_end-$timestamp_start);
+    $timestamp_end = strpos($return_string, ",", $timestamp_start);
+    $timestamp_string = mb_substr($return_string, $timestamp_start, $timestamp_end-$timestamp_start);
     $timestamp = intval($timestamp_string);
     if ($timestamp <= 0) $timestamp = intval(time());
     echo "timestamp";  
     echo $timestamp;
     $value = -9999.0;
     $value_start = $timestamp_end+1;
-    $value_end = strpos($data_string, ",", $value_start);
-    $value_string = mb_substr($data_string, $value_start, $value_end-$value_start);
+    $value_end = strpos($return_string, ",", $value_start);
+    $value_string = mb_substr($return_string, $value_start, $value_end-$value_start);
     $value = doubleval($value_string);
     echo "value";
     echo $value;
     $subsamples_string = "-9999";
     $subsamples_start = $value_end+1;
-    $subsamples_end = strpos($data_string, ",", $subsamples_start);
-    $subsamples_string = mb_substr($data_string, $subsamples_start, $subsamples_end-$subsamples_start);
+    $subsamples_end = strpos($return_string, ",", $subsamples_start);
+    $subsamples_string = mb_substr($return_string, $subsamples_start, $subsamples_end-$subsamples_start);
     echo "subsamples_string";
     echo $subsamples_string;
     $base64_string = "-9999";
     $base64_start = $subsamples_end+1;
-    $base64_end = strpos($data_string, ",", $base64_start);
-    $base64_string = mb_substr($data_string, $base64_start, $base64_end-$base64_start);
+    $base64_end = strpos($return_string, ",", $base64_start);
+    $base64_string = mb_substr($return_string, $base64_start, $base64_end-$base64_start);
     echo "base64_string";
     echo $base64_string;
 
@@ -104,14 +103,6 @@ while ($channel_start < $data_end)
 
 $conn->commit();
 $conn->close();
-
-
-function getPost($key, $default) 
-{
-  if (isset($_POST[$key]))
-    return $_POST[$key];
-  return $default;
-}
 
 
 ?>
