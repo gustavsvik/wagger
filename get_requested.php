470e84b6fe1b6f96403e296f66be2a7c1275c18f
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2019-10-05 17:58:02 +0000

    Update get_requested.php

--- get_requested.php
+++ get_requested.php
@@ -1,44 +1,38 @@
 <?php
 
+
 include("../db_ini.php");
-
-$data_string = NULL;
-$duration = NULL;
-$unit = NULL;
-$data_string = strval(getPost('channelrange', '-1;;'));
-$duration = intval(getPost('duration', 10));
-$unit = intval(getPost('unit', 1));
-
+include("header.php");
 
 $data_start = 0;
-$data_end = strlen($data_string);
+$data_end = strlen($request_string);
 $channel_start = $data_start;
 $channels_list = "(";
 while ($channel_start < $data_end)
 {
   if ($channel_start>0) $channels_list .= ",";
-  $channel_end = strpos($data_string, ';', $channel_start);
-  $channel_string = mb_substr($data_string, $channel_start, $channel_end-$channel_start);
+  $channel_end = strpos($request_string, ';', $channel_start);
+  $channel_string = mb_substr($request_string, $channel_start, $channel_end-$channel_start);
   $channel = intval($channel_string);
   $points_start = $channel_end+1;
-  $points_end = strpos($data_string, ';', $points_start);
-  $points_string = mb_substr($data_string, $points_start, $points_end-$points_start);
+  $points_end = strpos($request_string, ';', $points_start);
+  $points_string = mb_substr($request_string, $points_start, $points_end-$points_start);
   while ($points_start < $points_end)
   {
     $timestamp_start = $points_start;
-    $timestamp_end = strpos($data_string, ",", $timestamp_start);
-    $timestamp_string = mb_substr($data_string, $timestamp_start, $timestamp_end-$timestamp_start);
+    $timestamp_end = strpos($request_string, ",", $timestamp_start);
+    $timestamp_string = mb_substr($request_string, $timestamp_start, $timestamp_end-$timestamp_start);
     $timestamp = intval($timestamp_string);
     $value_start = $timestamp_end+1;
-    $value_end = strpos($data_string, ",", $value_start);
-    $value_string = mb_substr($data_string, $value_start, $value_end-$value_start);
+    $value_end = strpos($request_string, ",", $value_start);
+    $value_string = mb_substr($request_string, $value_start, $value_end-$value_start);
     $value = doubleval($value_string);
     $subsamples_start = $value_end+1;
-    $subsamples_end = strpos($data_string, ",", $subsamples_start);
-    $subsamples_string = mb_substr($data_string, $subsamples_start, $subsamples_end-$subsamples_start);
+    $subsamples_end = strpos($request_string, ",", $subsamples_start);
+    $subsamples_string = mb_substr($request_string, $subsamples_start, $subsamples_end-$subsamples_start);
     $base64_start = $subsamples_end+1;
-    $base64_end = strpos($data_string, ",", $base64_start);
-    $base64_string = mb_substr($data_string, $base64_start, $base64_end-$base64_start);
+    $base64_end = strpos($request_string, ",", $base64_start);
+    $base64_string = mb_substr($request_string, $base64_start, $base64_end-$base64_start);
     $points_start = $base64_end+1;
   }
   $channel_start = $points_end+1;
@@ -54,6 +48,7 @@ if ($conn->connect_errno)
   printf("Connect failed: %s\n", mysqli_connect_error());
   exit();
 }
+
 
 $sql_request_channels = "SELECT DISTINCT AD.CHANNEL_INDEX FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX IN " . $channels_list . " AND AD.STATUS = -1";
 $channels_requested = $conn->query($sql_request_channels);
@@ -83,16 +78,10 @@ else
 
 $conn->close();
 
+
 header("Content-type: application/json");
 $json_array = array('returnstring' => $return_string);
 echo json_encode($json_array);
 
-
-function getPost($key, $default) 
-{
-  if (isset($_POST[$key]))
-    return $_POST[$key];
-  return $default;
-}
 
 ?>
