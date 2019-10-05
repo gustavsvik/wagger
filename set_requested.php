a0b1a0b2e6c1ef07944defdaab89df30df3a9e70
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2019-10-05 17:59:07 +0000

    Update set_requested.php

--- set_requested.php
+++ set_requested.php
@@ -1,9 +1,8 @@
 <?php
 
-include("../db_ini.php");
 
-$data_string = NULL;
-$data_string = strval(getPost('returnstring', ';,,,;'));
+include("../db_ini.php");
+include("header.php");
 
 $conn = mysqli_init();
 if (!$conn) 
@@ -19,59 +18,40 @@ if (!$conn->real_connect($servername, $username, $password, $dbname))
   die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
 
-//$conn = new mysqli($servername, $username, $password, $dbname);
-//$conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
-//if (mysqli_connect_errno())
-//{
-//  printf("Connect failed: %s\n", mysqli_connect_error());
-//  exit();
-//}
-
 $conn->autocommit(FALSE);
 $conn->begin_transaction();
 
 
 $data_start = 0;
-$data_end = strlen($data_string);
+$data_end = strlen($return_string);
 $channel_start = $data_start;
 
 while ($channel_start < $data_end)
 {
-  $channel_end = strpos($data_string, ';', $channel_start);
-  $channel_string = mb_substr($data_string, $channel_start, $channel_end-$channel_start);
+  $channel_end = strpos($return_string, ';', $channel_start);
+  $channel_string = mb_substr($return_string, $channel_start, $channel_end-$channel_start);
   $channel = intval($channel_string);
-  //echo "channel";  
-  //echo $channel;
 
   $points_start = $channel_end+1;
-  $points_end = strpos($data_string, ';', $points_start);
-  $points_string = mb_substr($data_string, $points_start, $points_end-$points_start);
+  $points_end = strpos($return_string, ';', $points_start);
+  $points_string = mb_substr($return_string, $points_start, $points_end-$points_start);
 
   while ($points_start < $points_end)
   {
     $timestamp_start = $points_start;
-    $timestamp_end = strpos($data_string, ",", $timestamp_start);
-    $timestamp_string = mb_substr($data_string, $timestamp_start, $timestamp_end-$timestamp_start);
+    $timestamp_end = strpos($return_string, ",", $timestamp_start);
+    $timestamp_string = mb_substr($return_string, $timestamp_start, $timestamp_end-$timestamp_start);
     $timestamp = intval($timestamp_string);
-    //error_log("timestamp");  
-    //error_log($timestamp);
     $value_start = $timestamp_end+1;
-    $value_end = strpos($data_string, ",", $value_start);
-    $value_string = mb_substr($data_string, $value_start, $value_end-$value_start);
+    $value_end = strpos($return_string, ",", $value_start);
+    $value_string = mb_substr($return_string, $value_start, $value_end-$value_start);
     $value = doubleval($value_string);
-    //error_log("value");
-    //error_log($value);
     $subsamples_start = $value_end+1;
-    $subsamples_end = strpos($data_string, ",", $subsamples_start);
-    $subsamples_string = mb_substr($data_string, $subsamples_start, $subsamples_end-$subsamples_start);
-    //error_log("subsamples_string");
-    //error_log($subsamples_string);
+    $subsamples_end = strpos($return_string, ",", $subsamples_start);
+    $subsamples_string = mb_substr($return_string, $subsamples_start, $subsamples_end-$subsamples_start);
     $base64_start = $subsamples_end+1;
-    $base64_end = strpos($data_string, ",", $base64_start);
-    $base64_string = mb_substr($data_string, $base64_start, $base64_end-$base64_start);
-    //error_log("base64_string");
-    //error_log($base64_string);
-
+    $base64_end = strpos($return_string, ",", $base64_start);
+    $base64_string = mb_substr($return_string, $base64_start, $base64_end-$base64_start);
 
     $stmt=$conn->prepare("UPDATE " . $acquired_data_table_name . " SET ACQUIRED_VALUE=?,ACQUIRED_SUBSAMPLES=?,ACQUIRED_BASE64=?,STATUS=0 WHERE CHANNEL_INDEX=? AND ACQUIRED_TIME=? AND STATUS=-1");
     $stmt->bind_param('sssss',$value,$subsamples_string,$base64_string,$channel,$timestamp);
@@ -90,16 +70,6 @@ while ($channel_start < $data_end)
 
 $conn->commit();
 $conn->close();
-
-
-//print_r($return_array);
-
-function getPost($key, $default) 
-{
-  if (isset($_POST[$key]))
-    return $_POST[$key];
-  return $default;
-}
 
 
 ?>
