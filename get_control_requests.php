fac16538ffa180c58391c5248028b639388e2b3b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-02-22 20:46:24 +0000

    Update get_control_requests.php

--- get_control_requests.php
+++ get_control_requests.php
@@ -2,43 +2,11 @@
 
 
 include("../db_ini.php");
+include("../utils.php");
 include("header.php");
 
-$data_start = 0;
-$data_end = strlen($request_string);
-$channel_start = $data_start;
-$channels_list = "(";
-while ($channel_start < $data_end)
-{
-  if ($channel_start>0) $channels_list .= ",";
-  $channel_end = strpos($request_string, ';', $channel_start);
-  $channel_string = mb_substr($request_string, $channel_start, $channel_end-$channel_start);
-  $channel = intval($channel_string);
-  $points_start = $channel_end+1;
-  $points_end = strpos($request_string, ';', $points_start);
-  $points_string = mb_substr($request_string, $points_start, $points_end-$points_start);
-  while ($points_start < $points_end)
-  {
-    $timestamp_start = $points_start;
-    $timestamp_end = strpos($request_string, ",", $timestamp_start);
-    $timestamp_string = mb_substr($request_string, $timestamp_start, $timestamp_end-$timestamp_start);
-    $timestamp = intval($timestamp_string);
-    $value_start = $timestamp_end+1;
-    $value_end = strpos($request_string, ",", $value_start);
-    $value_string = mb_substr($request_string, $value_start, $value_end-$value_start);
-    $value = doubleval($value_string);
-    $subsamples_start = $value_end+1;
-    $subsamples_end = strpos($request_string, ",", $subsamples_start);
-    $subsamples_string = mb_substr($request_string, $subsamples_start, $subsamples_end-$subsamples_start);
-    $base64_start = $subsamples_end+1;
-    $base64_end = strpos($request_string, ",", $base64_start);
-    $base64_string = mb_substr($request_string, $base64_start, $base64_end-$base64_start);
-    $points_start = $base64_end+1;
-  }
-  $channel_start = $points_end+1;
-  $channels_list .= $channel_string;
-}
-$channels_list .= ")";
+
+$channels_list = getListByIDs($request_string);
 
 $return_string = NULL;
 
@@ -49,7 +17,7 @@ if (mysqli_connect_errno())
   exit();
 }
 
-$sql_request_channels = "SELECT DISTINCT AD.CHANNEL_INDEX FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX IN " . $channels_list . " AND AD.STATUS = -1";
+$sql_request_channels = "SELECT DISTINCT AD.CHANNEL_INDEX FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX IN (" . $channels_list . ") AND AD.STATUS = -1";
 $channels_requested = $conn->query($sql_request_channels);
 if ($channels_requested->num_rows > 0) 
 {
