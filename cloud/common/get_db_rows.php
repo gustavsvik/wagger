b7fc3e7c302e860861c7e88d4ffb03e55fbbebc3
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-02-23 05:42:52 +0000

    Add new file

---
+++ cloud/common/get_db_rows.php
@@ -0,0 +1,52 @@
+<?php
+
+
+include("../db_ini.php");
+include("../utils.php");
+
+
+$id_range = NULL;
+$id_range = strval(getPost('idrange', 'NULL'));
+$table_label = NULL;
+$table_label = strval(getPost('tablelabel', 'none'));
+
+//$rows_list = getListByIDs($id_range);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_errno)
+{
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+}
+
+$sql_request_channels = "SELECT * FROM t_" . strtolower($table_label) . " C WHERE C." . strtoupper($table_label) . "_" . "UNIQUE_INDEX IN (" . $id_range . ")";
+
+$ifp = fopen("/srv/websites/wagger/cloud/client/images/test.txt", 'wb'); 
+fwrite($ifp, " table_name:" . $table_name);
+fwrite($ifp, " id_range:" . $id_range);
+fwrite($ifp, " sql_request_channels:" . $sql_request_channels);
+fclose($ifp);
+
+
+$rows_requested = $conn->query($sql_request_channels); 
+
+if ($rows_requested->num_rows > 0) 
+{
+  while ($row = $rows_requested->fetch_assoc()) 
+  {
+    $rows[] = $row;
+  }
+}
+else 
+{
+}
+
+$conn->close();
+
+
+header("Content-type: application/json");
+$json_array = array('returnstring' => $rows);
+echo json_encode($json_array);
+
+
+?>
