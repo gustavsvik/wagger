7ad7bccdc80439ef7015bfab9c7d2940e227a269
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-04-25 11:43:16 +0000

    Upload New File

---
+++ cloud/client/header.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$channels= NULL;
+$start_time = NULL;
+$end_time = NULL;
+$duration = NULL;
+$unit = NULL;
+$value = NULL;
+$delete_horizon = NULL;
+$lowest_status = NULL;
+
+$latest_point_time = NULL;
+$points_range = NULL;
+$points_range_string = NULL;
+
+$channels = strval(getPost('channels', ";"));
+$start_time = intval(getPost('start_time', -9999));
+$end_time = intval(getPost('end_time', -9999));
+$duration = intval(getPost('duration', 10));
+$unit = intval(getPost('unit', 1));
+$value = floatval(getPost('value', -9999.0));
+$delete_horizon = intval(getPost('delete_horizon', 3600));
+$lowest_status = intval(getPost('lowest_status', 0));
+
+$channel_start = 0;
+$data_end = strlen($channels);
+
+
+?>
