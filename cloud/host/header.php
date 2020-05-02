3360271be5bf353404b26fcee1babe9e9a37ebef
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-02 12:50:11 +0000

    Update header.php

--- cloud/host/header.php
+++ cloud/host/header.php
@@ -5,11 +5,13 @@ $request_string = NULL;
 $return_string = NULL;
 $duration = NULL;
 $unit = NULL;
+$lowest_status = NULL;
 
 $request_string = strval(getPost('channelrange', '-1;;'));
 $return_string = strval(getPost('returnstring', ';,,,;'));
 $duration = intval(getPost('duration', 10));
 $unit = intval(getPost('unit', 1));
+$lowest_status = intval(getPost('lowest_status', -1));
 
 
 ?>
