bef4db5bd964f10a8e9a6a85e95c94b4c8ebb0d1
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-17 17:35:10 +0000

    Update send_request.php

--- cloud/client/send_request.php
+++ cloud/client/send_request.php
@@ -9,11 +9,19 @@ include("header.php");
 if ($data_end > 0)
 {
 
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  if (mysqli_connect_errno())
+  $conn = mysqli_init();
+
+  if (!$conn) 
   {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+    die('mysqli_init failed');
+  }
+  if (!$conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5)) 
+  {
+    die('Setting MYSQLI_OPT_CONNECT_TIMEOUT failed');
+  }
+  if (!$conn->real_connect($servername, $username, $password, $dbname)) 
+  {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
   }
 
   if ($start_time == -9999)
@@ -42,34 +50,51 @@ if ($data_end > 0)
     $channel_string = mb_substr($channels, $channel_start, $channel_end-$channel_start);
     $channel = intval($channel_string);
     $sql_delete = "DELETE FROM " . $acquired_data_table_name . " WHERE CHANNEL_INDEX=" . $channel_string . " AND TIMEDIFF(SYSDATE(), ADDED_TIMESTAMP)>" . strval($delete_horizon) . " AND STATUS IN (-1,0)";
-    if ($conn)
+
+    $conn->autocommit(FALSE);
+    $conn->begin_transaction();
+    $stmt=$conn->prepare($sql_delete);
+    if ($stmt)
+    {
+      if(!$stmt->execute())
+      {
+        $conn->rollback();
+        die();
+      }
+      $stmt->close();
+    }
+    $conn->commit();
+
+    $existing_points_array = array();
+    $sql_get_existing_points = "SELECT DISTINCT AD.ACQUIRED_TIME FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX=" . $channel_string . " AND AD.ACQUIRED_TIME IN" . $points_range_string;
+    $existing_points = $conn->query($sql_get_existing_points);
+    if ($existing_points->num_rows > 0)
+    {
+      while ($point_row = $existing_points->fetch_array(MYSQLI_NUM))
+      {
+        if (!is_null($point_row[0])) $existing_points_array[] = $point_row[0];
+      }
+    }
+
+    $at_least_one_missing = FALSE;
+    foreach ($points_range as $point)
+    {
+      if (in_array($point, $existing_points_array))
+      {
+      }
+      else
+      {
+        $at_least_one_missing = TRUE;
+      }
+    }
+
+    if ($at_least_one_missing)
     {
       $conn->autocommit(FALSE);
       $conn->begin_transaction();
-      $stmt=$conn->prepare($sql_delete);
-      if ($stmt)
-      {
-        if(!$stmt->execute())
-        {
-          $conn->rollback();
-          exit();
-        }
-        $stmt->close();
-      }
-      $conn->commit();
 
-      $existing_points_array = array();
-      $sql_get_existing_points = "SELECT DISTINCT AD.ACQUIRED_TIME FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX=" . $channel_string . " AND AD.ACQUIRED_TIME IN" . $points_range_string;
-      $existing_points = $conn->query($sql_get_existing_points);
-      if ($existing_points->num_rows > 0)
-      {
-        while ($point_row = $existing_points->fetch_array(MYSQLI_NUM))
-        {
-          if (!is_null($point_row[0])) $existing_points_array[] = $point_row[0];
-        }
-      }
+      $missing_points_string = " ";
 
-      $at_least_one_missing = FALSE;
       foreach ($points_range as $point)
       {
         if (in_array($point, $existing_points_array))
@@ -77,48 +102,25 @@ if ($data_end > 0)
         }
         else
         {
-          $at_least_one_missing = TRUE;
-        }
-      }
-
-      if ($at_least_one_missing)
-      {
-        $conn->autocommit(FALSE);
-        $conn->begin_transaction();
-
-        $missing_points_string = " ";
-
-        foreach ($points_range as $point)
-        {
-          if (in_array($point, $existing_points_array))
+          $stmt=$conn->prepare("INSERT INTO " . $acquired_data_table_name . " (ACQUIRED_TIME,CHANNEL_INDEX,ACQUIRED_VALUE,STATUS) VALUES (?,?,?,-1)");
+          if ($stmt)
           {
-          }
-          else
-          {
-            $stmt=$conn->prepare("INSERT INTO " . $acquired_data_table_name . " (ACQUIRED_TIME,CHANNEL_INDEX,ACQUIRED_VALUE,STATUS) VALUES (?,?,?,-1)");
-            if ($stmt)
+            $stmt->bind_param('iid',$point,$channel,$value);
+            if (!$stmt->execute())
             {
-              $stmt->bind_param('iid',$point,$channel,$value);
-              if (!$stmt->execute())
-              {
-                $conn->rollback();
-                exit();
-              }
-              $stmt->close();
+              $conn->rollback();
+              die();
             }
+            $stmt->close();
           }
         }
-
-        $conn->commit();
       }
-      
+      $conn->commit();
     }
-
     $channel_start = $channel_end+1;
-
   }
-
-  if ($conn) $conn->close();
+  
+  $conn->close();
 
 }
 
