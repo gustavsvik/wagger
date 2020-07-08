7c82647e9073df522980c0f3e59bc0eab9ae291a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-08 05:50:03 +0000

    Update utils.js

--- cloud/client/utils.js
+++ cloud/client/utils.js
@@ -1,6 +1,79 @@
 "use strict";
 
 
+
+class AppData
+{
+  constructor()
+  {
+    this.CANVAS = {};
+    this.CANVAS_POS_X = 250 ;
+    this.CANVAS_POS_Y = 10 ;
+    this.STD_SCALE_HEIGHT = 480;
+    this.WARNING_FONTSIZE = 36;
+    this.STANDARD_FONTSIZE = 12;
+    this.MAX_DISPLAY_DIGITS = 13;
+    this.STANDARD_FOREGROUND_COLOR = [255,255,255,255];
+    this.STANDARD_BACKGROUND_COLOR = [0,0,255,255];
+    this.FRAME_RATE = 5 ;
+    this.REQUEST_INTERVAL = 2 ;
+    this.NTP_SYNC_INTERVAL = 60 ;
+    this.TIME_ZONE = "UTC";
+    this.BROWSER_URL = window.location.hostname;
+    this.CLIENT_URL = "http://" + this.BROWSER_URL  + "/client/";
+    this.FILES_DIR = "images/";
+    this.FILES_URL = this.CLIENT_URL + this.FILES_DIR;
+    this.WAIT_MESSAGE = "Retrieving data...";
+    this.last_get = 5;
+    this.last_request = 0; 
+    this.last_time_sync = 0; 
+    this.display_index = 0;
+    this.display_timeout = 900 ;
+    this.display_is_static = false;
+    this.chan_index_string = "";
+    this.img_chan_index_string = "";
+    this.ctrl_chan_index_string = "";
+    this.img_url = "";
+    this.img = {};
+    this.img_height = this.STD_SCALE_HEIGHT;
+    this.img_width = 4/3 * this.img_height; 
+    this.start_time = -9999;
+    this.end_time = -9999;
+    this.time_bins = 10; 
+    this.time_bin_size = 1;
+    this.data_time_string = ""; 
+    this.data_timestamp = 0 ;
+    this.server_time_string = this.WAIT_MESSAGE ;
+    this.server_timestamp = 0 ;
+    this.frames_active = 0 ;
+    this.ntp = {}
+    this.ntp.t1 = 0 ;
+    this.ntp.t2 = 0 ;
+    this.ntp.t3 = 0 ;
+    this.ntp.t4 = 0 ;
+    this.ntp.adjustment = 0 ;
+    this.ntp.timestamp = 0 ;
+    this.ntp.time_string = 0 ;
+    this.canvas_shift_x = 0 ;
+    this.canvas_shift_y = 0 ;
+    this.moved_slider = null ;
+    this.pressed_send = null ;
+    this.hovered_clicked_label = null ;
+  }
+}
+
+
+
+class DisplayData
+{
+  constructor()
+  {
+    this.data = [];
+  }
+}
+
+
+
 function arrayElementsIn(arr)
 {
   if (!Array.isArray(arr) || !arr.length) return false;
