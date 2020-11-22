0cec4ec1bed8a0d303ad2a773fe2bc447d888601
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 21:32:31 +0000

    Update get_db_rows.php

--- cloud/maint/get_db_rows.php
+++ cloud/maint/get_db_rows.php
@@ -9,7 +9,8 @@ $id_range = strval(getPost("idrange", "NULL"));
 $table_label = NULL;
 $table_label = strval(getPost("tablelabel", "none"));
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
+
 if ($conn->connect_errno)
 {
   printf("Connect failed: %s\n", mysqli_connect_error());
@@ -19,11 +20,11 @@ if ($conn->connect_errno)
 $sql_request_channels = "SELECT * FROM t_" . strtolower($table_label);
 if ($id_range !== "-9999") $sql_request_channels .= " C WHERE C." . strtoupper($table_label) . "_" . "UNIQUE_INDEX IN (" . $id_range . ")";
 
-$ifp = fopen("/srv/wagger/cloud/client/images/test.txt", "wb"); 
-fwrite($ifp, " table_name:" . $table_name);
-fwrite($ifp, " id_range:" . $id_range);
-fwrite($ifp, " sql_request_channels:" . $sql_request_channels);
-fclose($ifp);
+//$ifp = fopen("/srv/wagger/cloud/client/images/test.txt", "wb"); 
+//fwrite($ifp, " table_name:" . $table_name);
+//fwrite($ifp, " id_range:" . $id_range);
+//fwrite($ifp, " sql_request_channels:" . $sql_request_channels);
+//fclose($ifp);
 
 $rows_requested = $conn->query($sql_request_channels); 
 
