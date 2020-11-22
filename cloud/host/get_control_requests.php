2769471b922dce2b9211b1201ddacd57ec23d2c0
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 21:34:57 +0000

    Update get_control_requests.php

--- cloud/host/get_control_requests.php
+++ cloud/host/get_control_requests.php
@@ -10,21 +10,21 @@ $channels_list = getListByIDs($request_string);
 
 $return_string = NULL;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
 if (mysqli_connect_errno())
 {
   printf("Connect failed: %s\n", mysqli_connect_error());
   exit();
 }
 
-$sql_request_channels = "SELECT DISTINCT AD.CHANNEL_INDEX FROM " . $acquired_data_table_name . " AD WHERE AD.CHANNEL_INDEX IN (" . $channels_list . ") AND AD.STATUS = -1";
+$sql_request_channels = "SELECT DISTINCT AD.CHANNEL_INDEX FROM " . $ACQUIRED_DATA_TABLE_NAME . " AD WHERE AD.CHANNEL_INDEX IN (" . $channels_list . ") AND AD.STATUS = -1";
 $channels_requested = $conn->query($sql_request_channels);
 if ($channels_requested->num_rows > 0) 
 {
   while ($channel_row = $channels_requested->fetch_assoc()) 
   {
     $return_string .= $channel_row["CHANNEL_INDEX"] . ";";
-    $sql_get_channel_points = "SELECT ACQUIRED_TIME,ACQUIRED_VALUE FROM " . $acquired_data_table_name . " AD WHERE AD.STATUS = -1 AND AD.CHANNEL_INDEX = " . $channel_row["CHANNEL_INDEX"] . " LIMIT 1000";
+    $sql_get_channel_points = "SELECT ACQUIRED_TIME,ACQUIRED_VALUE FROM " . $ACQUIRED_DATA_TABLE_NAME . " AD WHERE AD.STATUS = -1 AND AD.CHANNEL_INDEX = " . $channel_row["CHANNEL_INDEX"] . " LIMIT 1000";
     $channel_points = $conn->query($sql_get_channel_points);
     if ($channel_points->num_rows > 0) 
     {
