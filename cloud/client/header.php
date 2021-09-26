658667dd20d6c7893e5bed5866cdbd6970b4ab9d
dannil10 <daniel.nilsson@gustavsvik.eu>
2021-09-26 08:46:08 +0200

    Enable unlimited storage of user contributed image files

--- cloud/client/header.php
+++ cloud/client/header.php
@@ -1,14 +1,18 @@
 <?php
 
 
-$ACCESSIBLE_CHANNELS = array_merge( range(17,32), range(97,112), [140,143,144,145,146,147,160,162,163,180,202], range(600,602), range(61010,61012), [168,169,170,171,148,172,173,152], [2000,2001,2002,2003], [99999] ) ; 
-$ACCESSIBLE_CHANNELS_FROM = 20000;
-$CLEAR_REQUESTED_CHANNELS = array_merge( range(17,32), range(97,112), [140,160,180], range(600,602), range(2000,2003) ) ; 
-$ARMORED_BYTE_STRING_CHANNELS = array_merge( [1], range(144,145), [150,151,162,163,170,171,148,172,173,152], [99999] ) ; 
+$ACCESSIBLE_CHANNELS = array_merge( range(17,32), range(97,112), [140,143,144,145,146,147,160,162,163,180,202], range(600,602), range(61010,61012), [168,169,170,171,148,172,173,152], [2000,2001,2002,2003], [99999] ) ;
+$UNRESTRICTED_CHANNELS_FROM = 20000;
+$CLEAR_REQUESTED_CHANNELS = array_merge( range(17,32), range(97,112), [140,160,180], range(600,602), range(2000,2003) ) ;
+$ARMORED_BYTE_STRING_CHANNELS = array_merge( [1], range(144,145), [150,151,162,163,170,171,148,172,173,152], [99999] ) ;
 $MAX_FILES_PER_CHANNEL = 20 ;
 $WRITE_IMAGE_FILES = FALSE;
 $WRITE_VALUE_FILES = FALSE;
 
+
+$latest_point_time = NULL;
+$points_range = NULL;
+$points_range_string = NULL;
 
 $channels= NULL;
 $start_time = NULL;
@@ -19,9 +23,7 @@ $value = NULL;
 $delete_horizon = NULL;
 $lowest_status = NULL;
 
-$latest_point_time = NULL;
-$points_range = NULL;
-$points_range_string = NULL;
+$sql_like_condition = NULL;
 
 $host_hardware_id = NULL;
 $host_text_id = NULL;
@@ -49,6 +51,8 @@ $web_api_table_label = strval(getGet('web_api_table_label', 'device'));
 
 $channel_start = 0;
 $data_end = strlen($channels);
+
+$sql_like_condition = strval(getPost('sql_like_condition', NULL));
 
 $host_hardware_id = strval(getPost('host_hardware_id', NULL));
 $host_text_id = strval(getPost('host_text_id', NULL));
