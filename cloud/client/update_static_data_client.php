e07cd3f789152dc3066fac6ae5e1019e097695be
dannil10 <daniel.nilsson@gustavsvik.eu>
2021-12-12 17:09:17 +0100

    More relevant info in web app user module table record

--- cloud/client/update_static_data_client.php
+++ cloud/client/update_static_data_client.php
@@ -20,23 +20,19 @@ debug_log('$module_address: ', $module_address);
 //debug_log('$common_address: ', $common_address);
 debug_log('$common_description: ', $common_description);
 
-$channel_text_id = 'json';
+$channel_text_id = 'JSON';
 $channel_address = http_get_client_ip();
 
 
 if (is_null($module_address)) $module_id_string = http_get_client_id_string(["REQUEST_TIME_FLOAT", "REQUEST_TIME", "REMOTE_PORT", "CONTENT_LENGTH", 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'REMOTE_ADDR']);
 else $module_id_string = strval($module_address);
-
 debug_log('$module_id_string: ', $module_id_string);
-$module_hardware_id = md5($module_id_string);
-debug_log('$module_hardware_id: ', $module_hardware_id);
-$module_text_id = $module_hardware_id;
 
 if (is_null($module_address)) $channel_id_string = http_get_client_id_string(["REQUEST_TIME_FLOAT", "REQUEST_TIME", "REMOTE_PORT", "CONTENT_LENGTH"]) . ' ' . $channel_text_id;
-else $channel_id_string = strval($module_address) . ' ' . $channel_text_id;
+else $channel_id_string = strval($module_address) . ' ' . $channel_address;
 debug_log('$channel_id_string: ', $channel_id_string);
 $channel_hardware_id = md5($channel_id_string) ;
-debug_log('$channel_id_string: ', $channel_id_string);
+debug_log('$channel_hardware_id: ', $channel_hardware_id);
 //$channel_text_id = $channel_hardware_id;
 
 $conn = db_get_connection($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
@@ -49,6 +45,9 @@ if (!is_null($conn))
   //db_update_static_by_index($conn, "HOST", $host_unique_index, $host_hardware_id, $host_text_id, $common_address, $common_description);
   //$device_unique_index = db_get_index($conn, "DEVICE", "HARDWARE_ID", $device_hardware_id);
   $module_unique_index = NULL; // Auto-incremented table
+  $module_hardware_id = md5($module_id_string);
+  debug_log('$module_hardware_id: ', $module_hardware_id);
+  $module_text_id = 'WEB'; //$module_hardware_id;
   $new_module_index = db_update_static_by_index($conn, "MODULE", $module_unique_index, $module_hardware_id, $module_text_id, $module_address, $common_description);
   //$device_unique_index = db_get_index($conn, "MODULE", "HARDWARE_ID", $module_hardware_id);
   //db_update_static_by_index($conn, "MODULE", $module_unique_index, $module_hardware_id, $module_text_id, $common_address, $common_description);
