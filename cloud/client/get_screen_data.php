a7c2dd06b371efe746c642cc00f1c18fef8ac204
dannil10 <daniel.nilsson@gustavsvik.eu>
2021-12-28 15:49:04 +0100

    Get screen layout filename in local  header.php

--- cloud/client/get_screen_data.php
+++ cloud/client/get_screen_data.php
@@ -1,11 +1,11 @@
 <?php
 
 
-include("../utils.php");
+include("header.php");
 
 
-$screen_layout_file= NULL;
-$screen_layout_file = strval(getGet('screen_layout_file', 'screens.json'));
+//$screen_layout_file= NULL;
+//$screen_layout_file = strval(getGet('screen_layout_file', 'screens.json'));
 
 $str = file_get_contents(__DIR__ . '/' . $screen_layout_file);
 
