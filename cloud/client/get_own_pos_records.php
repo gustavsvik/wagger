73b50b23cbb270e7d526ef5c7e77afc8b7f3da92
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-03-18 16:28:15 +0100

    Test of new server APIs

--- cloud/client/get_own_pos_records.php
+++ cloud/client/get_own_pos_records.php
@@ -1,6 +1,77 @@
 <?php
 
 
+include_once("../Log.php");
+include_once("../CheckIf.php");
+include_once("../GetSafe.php");
+include_once("../Transform.php");
+include_once("../AcquiredRecordsSql.php");
+include_once("../ClientAcquiredRecordsApi.php");
+
+$api = new ClientAcquiredRecordsApi();
+
+$channel_array = $api::get_channel_array();
+
+$lowest_status = 0 ;
+
+$sql = new AcquiredRecordsSql();
+$latest_stored_update_time = time(); //$sql->get_latest_channel_update_time($channel_array, $lowest_status); // time(); //db_get_max_value($conn, $table_name_string, $column_name_string);
+
+$api::update_time_horizon($latest_stored_update_time);
+
+$start_time = $api::get_start_time();
+$end_time = $api::get_end_time();
+
+$acquired_records = $sql->get_by_channels_time_range_status_range(['ACQUIRED_TIME','ACQUIRED_BYTES'], $channel_array, $start_time, $end_time, $lowest_status);
+
+Log::debug('$acquired_records: ', $acquired_records);
+$acquired_string = "";
+foreach ($acquired_records as $channel_data_array)
+{
+  $channel_string = Transform::to_string( ( GetSafe::by_key($channel_data_array, 0) ) );
+  $acquired_string .= $channel_string . ";";
+  $data_array = GetSafe::by_key($channel_data_array, 1);
+  foreach ($data_array as $record_row)
+  {
+    $time_string = Transform::to_string( ( GetSafe::by_key($record_row, 'ACQUIRED_TIME') ) );
+    $bytes_string = Transform::to_string( ( GetSafe::by_key($record_row, 'ACQUIRED_BYTES') ) );
+    $acquired_string .= $time_string . ",,," ;
+    $all_bytes_string = "[";
+    $bytes_string_json_array = json_decode($bytes_string, true);
+    if (CheckIf::is_iterable($bytes_string_json_array)) //(is_array($bytes_string_json_array) || $bytes_string_json_array instanceof Traversable)
+    {
+      foreach ($bytes_string_json_array as $bytes_string_json)
+      {
+        $message_json = $bytes_string_json[3];
+        $lon = number_format( (float)GetSafe::by_key($message_json, "lon"), 5, '.', '' );
+        $lat = number_format( (float)GetSafe::by_key($message_json, "lat"), 5, '.', '' );
+        $speed = number_format( (float)GetSafe::by_key($message_json, "speed"), 1, '.', '' );
+        $heading = number_format( (float)GetSafe::by_key($message_json, "heading"), 1, '.', '' );
+        $mmsi = $bytes_string_json[2];
+
+        $reduced_json_string = "";
+        if ( is_numeric($lon) && is_numeric($lat) )
+        {
+          $reduced_json_string = json_encode( array( "type" => 1, "mmsi" => $mmsi, "lon" => $lon, "lat" => $lat, "speed" => $speed, "heading" => $heading  ) );
+        }
+        if (strlen($reduced_json_string) > 0) $all_bytes_string .= $reduced_json_string . ',' ;
+      }
+      $acquired_string .= Transform::armored_from_separated_string( substr( $all_bytes_string, 0, -1 ) . "]" );
+    }
+    $acquired_string .= "," ;
+  }
+  $acquired_string .= ";";
+}
+Log::debug('$acquired_string: ', $acquired_string);
+
+$sql->close();
+
+header("Content-type: application/json");
+$json_array = array('returnstring' => $acquired_string) ; //, 'receivetime' => $receive_timestamp, 'transmittime' => $transmit_timestamp);
+echo json_encode($json_array);
+
+
+/*
 include_once("../header.php");
 include_once("../db_ini.php");
 include_once("../CheckIf.php");
@@ -57,22 +128,20 @@ foreach ($channel_array as $channel)
   $return_string .= $channel_string . ";";
 
   $sql_get_all_available_values = "SELECT T.ACQUIRED_TIME,T.ACQUIRED_BYTES FROM t_acquired_data T";
-  $sql_get_available_values = $sql_get_all_available_values . " WHERE " ;
-  $sql_get_available_values .= "T.CHANNEL_INDEX=" . $channel_string . " AND ";
+  $sql_get_all_available_values .= " WHERE T.CHANNEL_INDEX=" . $channel_string ;
+  $sql_get_available_values = $sql_get_all_available_values . " AND ";
   if (!$select_all_in_channel) $sql_get_available_values .= "T.ACQUIRED_TIME BETWEEN " . strval($start_time) . " AND ". strval($end_time) . " AND ";
   $sql_get_available_values .= "T.STATUS >= " . strval($lowest_status) . " AND T.STATUS < " . strval($STATUS_ARCHIVED) . " ORDER BY T.ACQUIRED_TIME DESC";
   debug_log('$sql_get_available_values: ', $sql_get_available_values);
   $available_values = $conn->query($sql_get_available_values);
 
-  if (is_object($available_values))
-  {
-    if ($available_values->num_rows <= 0)
+    if (is_object($available_values) && $available_values->num_rows <= 0)
     {
-        $sql_get_stored_archived_values = $sql_get_all_available_values . " AND T.STATUS >= " . strval($STATUS_ARCHIVED) . " ORDER BY T.ACQUIRED_TIME DESC";
+      $sql_get_stored_archived_values = $sql_get_all_available_values . " AND T.STATUS >= " . strval($STATUS_ARCHIVED) . " ORDER BY T.ACQUIRED_TIME DESC";
       debug_log('$sql_get_stored_archived_values: ', $sql_get_stored_archived_values);
       $available_values = $conn->query($sql_get_stored_archived_values);
     }
-    if ($available_values->num_rows > 0)
+    if (is_object($available_values) && $available_values->num_rows > 0)
     {
       while ($value_row = $available_values->fetch_array(MYSQLI_NUM))
       {
@@ -112,7 +181,6 @@ foreach ($channel_array as $channel)
     $return_string .= ";";
 
   }
-}
 
 $conn->close();
 
@@ -120,3 +188,4 @@ debug_log('$return_string: ', $return_string);
 header("Content-type: application/json");
 $json_array = array('returnstring' => $return_string) ; //, 'receivetime' => $receive_timestamp, 'transmittime' => $transmit_timestamp);
 echo json_encode($json_array);
+*/
