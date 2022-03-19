c9a78a64e1e5add541f56bfd1eaa096a4474863e
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-03-19 20:27:38 +0100

    Enforce strict typing (PHP 7), add default value arguments

--- cloud/GetSafe.php
+++ cloud/GetSafe.php
@@ -1,24 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
 
 class GetSafe
 {
 
 
-  public static function by_key($object, $key)
+  public static function by_key($object, $key, $default = NULL)
   {
-    return !empty($object[$key]) ? $object[$key] : NULL ;
+    return !empty($object[$key]) ? $object[$key] : $default ;
   }
 
 
-  public static function post($key, $default)
+  public static function post($key, $default = NULL)
   {
     if ( isset($_POST[$key]) ) return $_POST[$key];
     return $default;
   }
 
 
-  public static function get($key, $default)
+  public static function get($key, $default = NULL)
   {
     if ( isset($_GET[$key]) ) return $_GET[$key];
     return $default;
