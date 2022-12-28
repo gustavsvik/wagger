b3575a7bfeb57cdc1257f2eeda77cb98c5b2d1e9
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-12-28 17:09:21 +0100

    Map multiple identifiers to a single MIME type

--- cloud/Check.php
+++ cloud/Check.php
@@ -8,20 +8,23 @@ class Check
   public static function image_mime_type($imagedata)
   {
     $imagemimetypes = array(
-      "jpg" => "FFD8",
-      "png" => "89504E470D0A1A0A",
-      "gif" => "474946",
-      "bmp" => "424D",
-      "tiff" => "4949",
-      "tiff" => "4D4D",
-      "mp4" => "6674797069736F6D",
-      "3gp" => "000000186674797033677034"
+      "jpg" => ["FFD8"],
+      "png" => ["89504E470D0A1A0A"],
+      "gif" => ["474946"],
+      "bmp" => ["424D"],
+      "tiff" => ["4949", "4D4D"],
+      "mp4" => ["6674797069736F6D"],
+      "3gp" => ["000000186674797033677034"]
     );
 
-    foreach ($imagemimetypes as $mime => $hexbytes)
+    foreach ($imagemimetypes as $mime => $hexbytes_array)
     {
-      $bytes = get_bytes_from_hex_string($hexbytes);
-      if (substr($imagedata, 0, strlen($bytes)) == $bytes) return $mime;
+      foreach ($hexbytes_array as $hexbytes) 
+      {
+        $bytes = get_bytes_from_hex_string($hexbytes);
+        if (substr($imagedata, 0, strlen($bytes)) == $bytes)
+          return $mime;
+      }
     }
 
     return NULL;
