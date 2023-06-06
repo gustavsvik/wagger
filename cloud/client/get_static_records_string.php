ac2aa62f33ea8748ccb91998a93e79f585245faf
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-06-06 11:55:11 +0200

    Transfer device status via web api, some cleanup

--- cloud/client/get_static_records_string.php
+++ cloud/client/get_static_records_string.php
@@ -19,31 +19,32 @@ $sql = new StaticRecordsSql();
 
 
 $web_api_table_label = 'device'; //$api::get_web_api_table_label();
-$web_api_table_name = strtolower(strval($web_api_table_label));
-$web_api_table_name_string = "t_" . $web_api_table_label ;
-$web_api_column_label_string = strtoupper(strval($web_api_table_label));
+//$web_api_table_name = strtolower(strval($web_api_table_label));
+//$web_api_table_name_string = "t_" . $web_api_table_label ;
+//$web_api_column_label_string = strtoupper(strval($web_api_table_label));
 
-$browser_client_table_label = 'module';
-$browser_client_table_name = strtolower(strval($browser_client_table_label));
-$browser_client_table_name_string = "t_" . $browser_client_table_label ;
-$browser_client_column_label_string = strtoupper(strval($browser_client_table_label));
+//$browser_client_table_label = 'module';
+//$browser_client_table_name = strtolower(strval($browser_client_table_label));
+//$browser_client_table_name_string = "t_" . $browser_client_table_label ;
+//$browser_client_column_label_string = strtoupper(strval($browser_client_table_label));
 
 $start_time = -9999 ;
 $end_time = -9999 ;
 $duration = 1200 ;
 $unit = 1 ;
 
-$api::set_start_time($start_time) ;
-$api::set_end_time($end_time) ;
-$api::set_duration($duration) ;
-$api::set_unit($unit) ;
+//$api::set_start_time($start_time) ;
+//$api::set_end_time($end_time) ;
+//$api::set_duration($duration) ;
+//$api::set_unit($unit) ;
 
 $lowest_status = Status::FULFILLED ;
 $highest_status = Status::STORED ;
+$persistent_status = Status::STORED ;
 
 $return_string = "";
 
-$return_data_array = $sql::get_static_by_time_interval_status($web_api_table_label, $start_time, $duration, $unit, $end_time, $lowest_status, $highest_status );
+$return_data_array = $sql::get_static_by_time_interval_status($web_api_table_label, $start_time, $duration, $unit, $end_time, $lowest_status, $highest_status, $persistent_status );
 $log::debug('$return_data_array: ', $return_data_array);
 
 $dummy_channel_string = "0";
@@ -54,19 +55,25 @@ $times = $get_safe::by_key($return_data_array, 'times');
 $log::debug('$times: ', $times);
 $descriptions = $get_safe::by_key($return_data_array, 'descriptions');
 $log::debug('$descriptions: ', $descriptions);
+$statuses = $get_safe::by_key($return_data_array, 'statuses');
+$log::debug('$statuses: ', $statuses);
 
 if ( $check_if::is_iterable($times) && $check_if::is_iterable($descriptions)) // ( (is_array($times) || $times instanceof Traversable) && (is_array($descriptions) || $descriptions instanceof Traversable) )
 {
   foreach ( range( 0, count($times) - 1 ) as $index )
   {
-    $time_string = "";
     $time = $times[$index];
+    $time_string = "";
     if (!is_null($time)) $time_string = strval($time);
     if (strlen($time_string) > 0) $return_string .= $time_string ;
     $return_string .= ",,,";
 
+    $description_json = json_decode($descriptions[$index], true);
+    $status_key = $web_api_table_label . "_status";
+    $description_json[$status_key] = $statuses[$index];
+    $description = json_encode($description_json);
+
     $description_string = "";
-    $description = $descriptions[$index];
     if (!is_null($description)) $description_string = strval($description);
     if (strlen($description_string) > 0) $return_string .= $tr::armored_from_separated_string($description_string) ;
 
