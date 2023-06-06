ed241ae033907752320b2ba3486665761d49d550
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-06-06 18:31:22 +0200

    Take care of JS Infinity values in JSON

--- cloud/client/get_static_records_string.php
+++ cloud/client/get_static_records_string.php
@@ -10,7 +10,7 @@ include_once("../StaticRecordsApi.php");
 include_once("../StaticRecordsSql.php");
 
 
-$log = new Log();
+//$log = new Log();
 $check_if = new CheckIf();
 $get_safe = new GetSafe();
 $tr = new Transform();
@@ -45,18 +45,18 @@ $persistent_status = Status::STORED ;
 $return_string = "";
 
 $return_data_array = $sql::get_static_by_time_interval_status($web_api_table_label, $start_time, $duration, $unit, $end_time, $lowest_status, $highest_status, $persistent_status );
-$log::debug('$return_data_array: ', $return_data_array);
+//Log::debug('$return_data_array: ', $return_data_array);
 
 $dummy_channel_string = "0";
 $dummy_channel = intval($dummy_channel_string);
 $return_string .= $dummy_channel_string . ";";
 
 $times = $get_safe::by_key($return_data_array, 'times');
-$log::debug('$times: ', $times);
+//Log::debug('$times: ', $times);
 $descriptions = $get_safe::by_key($return_data_array, 'descriptions');
-$log::debug('$descriptions: ', $descriptions);
+//Log::debug('$descriptions: ', $descriptions);
 $statuses = $get_safe::by_key($return_data_array, 'statuses');
-$log::debug('$statuses: ', $statuses);
+//Log::debug('$statuses: ', $statuses);
 
 if ( $check_if::is_iterable($times) && $check_if::is_iterable($descriptions)) // ( (is_array($times) || $times instanceof Traversable) && (is_array($descriptions) || $descriptions instanceof Traversable) )
 {
@@ -68,13 +68,16 @@ if ( $check_if::is_iterable($times) && $check_if::is_iterable($descriptions)) //
     if (strlen($time_string) > 0) $return_string .= $time_string ;
     $return_string .= ",,,";
 
-    $description_json = json_decode($descriptions[$index], true);
-    $status_key = $web_api_table_label . "_status";
-    $description_json[$status_key] = $statuses[$index];
-    $description = json_encode($description_json);
-
     $description_string = "";
-    if (!is_null($description)) $description_string = strval($description);
+    if ($descriptions[$index])
+    {
+      $description_json = json_decode(preg_replace('.Infinity.', '"Infinity"', $descriptions[$index]), true);
+      $status_key = $web_api_table_label . "_status";
+      $description_json[$status_key] = $statuses[$index];
+      $description = json_encode($description_json);
+      $description_string = preg_replace('."Infinity".', 'Infinity', $description);
+    }
+    Log::debug('$description_string', $description_string);
     if (strlen($description_string) > 0) $return_string .= $tr::armored_from_separated_string($description_string) ;
 
     $return_string .= ",";
@@ -82,7 +85,7 @@ if ( $check_if::is_iterable($times) && $check_if::is_iterable($descriptions)) //
 }
 $return_string .= ";";
 
-$log::debug('$return_string: ', $return_string);
+Log::debug('$return_string: ', $return_string);
 
 header("Content-type: application/json");
 $json_array = array('returnstring' => $return_string) ; //, 'receivetime' => $receive_timestamp, 'transmittime' => $transmit_timestamp);
