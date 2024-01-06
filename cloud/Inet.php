8b698e7b4d1e640a29fdb6732cc22f45d88c4016
dannil10 <daniel.nilsson@gustavsvik.eu>
2024-01-07 00:28:31 +0100

    Extract client fingerprint with exclusion option

--- cloud/Inet.php
+++ cloud/Inet.php
@@ -4,7 +4,7 @@
 class Inet
 {
 
-    public static function http_get_client_ip()
+    public static function extract_client_ip()
     {
       $ip = NULL;
       if (!empty($_SERVER['HTTP_CLIENT_IP']))
@@ -21,5 +21,18 @@ class Inet
       }
       return $ip;
     }
+
+    public static function generate_client_id_string($exclude_keys)
+    {
+      $exclude_keys_array = [];
+      if (!is_null($exclude_keys)) $exclude_keys_array = $exclude_keys ;
+      $client_id_string = "";
+      foreach ($_SERVER as $server_key => $server_value)
+      {
+        if ( !( in_array($server_key, $exclude_keys_array) ) ) $client_id_string .= $server_key . ' ' . $server_value . ' ';
+      }
+      return $client_id_string;
+    }
+
 
 }
